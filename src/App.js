cc6bd3c59e1786e4f811ecb9d24c134c1578b8c4
S-Furman <szymon.fur@o2.pl>
2021-05-19 23:30:56 +0200

    Illustration added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Layout from "./components/Layout/Layout";
 import "./App.css";
 
 import Cards from "./components/Cards/Cards";
+import Illustration from "./components/Illustration/Illustration";
 
 function App() {
   return (
     <Layout>
       <div className="App">
         <Cards />
+        <Illustration />
       </div>
     </Layout>
   );
